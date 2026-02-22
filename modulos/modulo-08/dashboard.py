8971ae9f47b493855aaf979b58efb86992477a19
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-02-22 20:55:11 -0300

    refactor: use native Streamlit charts

--- modulos/modulo-08/dashboard.py
+++ modulos/modulo-08/dashboard.py
@@ -2,19 +2,18 @@
 This is the Streamlit Dashboard entrypoint.
 
 
-To run this app (on root folder):
-- Activate the virtual environment (.venv)
+To run this app:
+- On root folder, activate the .venv
 - Run: $ streamlit modulos/modulo-08/dashboard.py
 """
 
 
+import pandas as pd
 import streamlit as st
 from mysql_service import MySqlService
-from chart_factory import ChartFactory
 
 
 mysql_service = MySqlService()
-chart_factory = ChartFactory()
 
 
 st.set_page_config(
@@ -26,28 +25,28 @@ st.title("📊 PlusMarket Dashboard")
 
 
 # ----- Section 01 -----
-st.subheader("🧮 TOTAL DE PRODUTOS")
+st.subheader("🧮 Total de Produtos")
 row1_col1, row2_col2, row3_col3 = st.columns(3)
-total_items_magalu = mysql_service.get_total_products_by_marketplace(marketplace="magalu")
-total_items_ml = mysql_service.get_total_products_by_marketplace(marketplace="mercado_livre")
-total_items = total_items_magalu + total_items_ml
 
 with row1_col1:
+    total_items_magalu = mysql_service.get_total_products(marketplace="magalu")
     st.metric(
-        label="🔵 MAGAZINE LUIZA",
-        value=f"{total_items_magalu:,}"
+        label="🔵 Magazine Luiza",
+        value=f"{total_items_magalu:,}".replace(',', '.')
     )
 
 with row2_col2:
+    total_items_ml = mysql_service.get_total_products(marketplace="mercado_livre")
     st.metric(
-        label="🟡 MERCADO LIVRE",
-        value=f"{total_items_ml:,}"
+        label="🟡 Mercado Livre",
+        value=f"{total_items_ml:,}".replace(',', '.')
     )
 
 with row3_col3:
+    total_items = total_items_magalu + total_items_ml
     st.metric(
-        label="🔴 TOTAL",
-        value=f"{total_items:,}"
+        label="⚪️ Total",
+        value=f"{total_items:,}".replace(',', '.')
     )
 
 
@@ -55,207 +54,216 @@ st.divider()
 
 
 # ----- Section 02 -----
-st.subheader("💰 MÉDIA DE PREÇO POR CATEGORIA")
-row2_col1, row2_col2 = st.columns(2, gap="large")
-with row2_col1:
-    st.markdown("#### 🔵 MAGAZINE LUIZA")
-    magalu_items = mysql_service.get_avg_price_by_product(marketplace="magalu")
-    magalu_avg_price_fig = chart_factory.create_bar_chart(
-        xlabel="price",
-        ylabel="title",
-        display_xlabel="Preço Médio",
-        display_ylabel="Categoria",
-        chart_title="",
-        source=magalu_items
-    )
-    st.pyplot(magalu_avg_price_fig, width="stretch")
+st.subheader("📦 Total de Produtos por Categoria")
+row3_col1, row3_col2, row3_col3 = st.columns(3, gap="medium")
 
-with row2_col2:
-    st.markdown("#### 🟡 MERCADO LIVRE")
-    ml_items = mysql_service.get_avg_price_by_product(marketplace="mercado_livre")
-    ml_avg_price_fig = chart_factory.create_bar_chart(
-        xlabel="price",
-        ylabel="title",
-        display_xlabel="Preço Médio",
-        display_ylabel="Categoria",
-        chart_title="",
-        source=ml_items
+with row3_col1:
+    st.markdown("#### 🔵 Magazine Luiza")
+    total_by_category_magalu = mysql_service.get_total_products_by_category_by_marketplace(
+        marketplace="magalu"
     )
-    st.pyplot(ml_avg_price_fig, width="stretch")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=total_by_category_magalu),
+        x="category",
+        y="total",
+        x_label="Categoria",
+        y_label="Total",
+        sort="total",
+        color="#1E90FF"  # dodger blue
+    )
+
+
+with row3_col2:
+    st.markdown("#### 🟡 Mercado Livre")
+    total_by_category_ml = mysql_service.get_total_products_by_category_by_marketplace(
+        marketplace="mercado_livre"
+    )
+
+    st.bar_chart(
+        data=pd.DataFrame(data=total_by_category_ml),
+        x="category",
+        y="total",
+        x_label="Categoria",
+        y_label="Total",
+        sort="total",
+        color="#FFEE8C"  # pastel yellow
+    )
+
+with row3_col3:
+    st.markdown("#### ⚪️ Total")
+    total_items_by_category = mysql_service.get_total_products_by_category()
+
+    st.bar_chart(
+        data=pd.DataFrame(data=total_items_by_category),
+        x="category",
+        y="total",
+        x_label="Categoria",
+        y_label="Total",
+        sort="total",
+        color="#FFFFFF"  # white
+    )
 
 
 st.divider()
 
 
 # ----- Section 03 -----
-st.subheader("📦 DISTRIBUIÇÃO POR CATEGORIA")
-row3_col1, row3_col2, row3_col3 = st.columns(3, gap="medium")
+st.subheader("💰 Média de Preço por Categoria")
+row2_col1, row2_col2 = st.columns(2, gap="large")
 
-with row3_col1:
-    st.markdown("#### 🔵 MAGAZINE LUIZA")
-    total_by_category_magalu = mysql_service.get_total_products_by_category_by_marketplace(
-        marketplace="magalu"
-    )
-    total_by_category_magalu_fig = chart_factory.create_pie_chart(
-        xlabel="total",
-        ylabel="category",
-        chart_title="",
-        source=total_by_category_magalu
-    )
-    st.pyplot(total_by_category_magalu_fig, width="stretch")
+with row2_col1:
+    st.markdown("#### 🔵 Magazine Luiza")
+    magalu_items = mysql_service.get_avg_category_prices(marketplace="magalu")
 
-with row3_col2:
-    st.markdown("#### 🟡 MERCADO LIVRE")
-    total_by_category_ml = mysql_service.get_total_products_by_category_by_marketplace(
-        marketplace="mercado_livre"
+    st.bar_chart(
+        data=pd.DataFrame(data=magalu_items),
+        x="category",
+        y="price",
+        x_label="Categoria",
+        y_label="Preço Médio",
+        sort="price",
+        color="#1E90FF"  # dodger blue
     )
-    total_by_category_ml_fig = chart_factory.create_pie_chart(
-        xlabel="total",
-        ylabel="category",
-        chart_title="",
-        source=total_by_category_ml
-    )
-    st.pyplot(total_by_category_ml_fig, width="stretch")
 
-with row3_col3:
-    st.markdown("#### 🔴 TOTAL")
-    total_items_by_category = mysql_service.get_total_products_by_category()
-    total_items_by_category_fig = chart_factory.create_pie_chart(
-        xlabel="total",
-        ylabel="category",
-        chart_title="",
-        source=total_items_by_category
+with row2_col2:
+    st.markdown("#### 🟡 Mercado Livre")
+    ml_items = mysql_service.get_avg_category_prices(marketplace="mercado_livre")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=ml_items),
+        x="category",
+        y="price",
+        x_label="Categoria",
+        y_label="Preço Médio",
+        sort="price",
+        color="#FFEE8C"  # pastel yellow
     )
-    st.pyplot(total_items_by_category_fig, width="stretch")
 
 
 st.divider()
 
 
 # ----- Section 04 -----
-st.subheader("🏆 TOP 3 PRODUTOS MAIS BEM AVALIADOS")
+st.subheader("🏆 Top 3 Produtos mais bem Avaliados")
 row4_col1, row4_col2 = st.columns(2, gap="large")
 
 with row4_col1:
-    st.markdown("#### 🔵 MAGAZINE LUIZA")
-    top3_magalu = mysql_service.get_top3_product_rating(marketplace="magalu")
-    top3_magalu_fig = chart_factory.create_bar_chart(
-        xlabel="rating",
-        ylabel="product",
-        display_xlabel="Avaliação",
-        display_ylabel="Produto",
-        chart_title="",
-        source=top3_magalu,
+    st.markdown("#### 🔵 Magazine Luiza")
+    top3_magalu = mysql_service.get_top3_best_product_rating(marketplace="magalu")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=top3_magalu),
+        x="product",
+        y="rating",
+        x_label="Produto",
+        y_label="Avaliação",
+        sort="rating",
+        color="#1E90FF",  # dodger blue
+        horizontal=True
     )
-    st.pyplot(top3_magalu_fig, width="stretch")
 
 with row4_col2:
-    st.markdown("#### 🟡 MERCADO LIVRE")
-    top3_ml = mysql_service.get_top3_product_rating(marketplace="mercado_livre")
-    top3_ml_fig = chart_factory.create_bar_chart(
-        xlabel="rating",
-        ylabel="product",
-        display_xlabel="Avaliação",
-        display_ylabel="Produto",
-        chart_title="",
-        source=top3_ml
+    st.markdown("#### 🟡 Mercado Livre")
+    top3_ml = mysql_service.get_top3_best_product_rating(marketplace="mercado_livre")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=top3_ml),
+        x="product",
+        y="rating",
+        x_label="Produto",
+        y_label="Avaliação",
+        sort="rating",
+        color="#FFEE8C",  # pastel yellow
+        horizontal=True
     )
-    st.pyplot(top3_ml_fig, width="stretch")
 
 
 st.divider()
 
 
 # ----- Section 05 -----
-st.subheader("🤑 TOP 5 PRODUTOS MAIS CAROS")
+st.subheader("🤑 Top 5 Produtos mais Caros")
 row5_col1, row5_col2 = st.columns(2, gap="large")
 
 with row5_col1:
-    st.markdown("#### 🔵 MAGAZINE LUIZA")
-    top5_magalu = mysql_service.get_top5_most_expensive_products_by_marketplace(marketplace="magalu")
-    top5_magalu_fig = chart_factory.create_bar_chart(
-        xlabel="price",
-        ylabel="product",
-        display_xlabel="Produto",
-        display_ylabel="Preço",
-        chart_title="",
-        source=top5_magalu,
-        horizontal_bars=False
+    st.markdown("#### 🔵 Magazine Luiza")
+    top5_magalu = mysql_service.get_top5_expensive_products_prices(marketplace="magalu")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=top5_magalu),
+        x="product",
+        y="price",
+        x_label="Produto",
+        y_label="Preço",
+        sort="price",
+        color="#1E90FF",  # dodger blue
+        horizontal=True
     )
-    st.pyplot(top5_magalu_fig, width="stretch")
 
 with row5_col2:
-    st.markdown("#### 🟡 MERCADO LIVRE")
-    top5_ml = mysql_service.get_top5_most_expensive_products_by_marketplace(marketplace="mercado_livre")
-    top5_ml_fig = chart_factory.create_bar_chart(
-        xlabel="price",
-        ylabel="product",
-        display_xlabel="Produto",
-        display_ylabel="Preço",
-        chart_title="",
-        source=top5_ml,
-        xlabel_size=5,
-        horizontal_bars=False
+    st.markdown("#### 🟡 Mercado Livre")
+    top5_ml = mysql_service.get_top5_expensive_products_prices(marketplace="mercado_livre")
+
+    st.bar_chart(
+        data=pd.DataFrame(data=top5_magalu),
+        x="product",
+        y="price",
+        x_label="Produto",
+        y_label="Preço",
+        sort="price",
+        color="#FFEE8C",  # pastel yellow
+        horizontal=True
     )
-    st.pyplot(top5_ml_fig, width="stretch")
 
 
 st.divider()
 
 
 # ----- Section 06 -----
-st.subheader("📈 VARIAÇÃO DE PREÇO MÉDIO POR CATEGORIA")
+st.subheader("📈 Variação de Preço Médio por Categoria")
 st.markdown("💡 DICA: Utilize a caixa de seleção ao lado")
-selectbox_options = ["Caixa de Som", "Celular", "Fone", "Notebook", "Smartwatch", "Tablet", "Teclado", "TV"]
-selected_category = st.sidebar.selectbox("CATEGORIAS", selectbox_options, index=0)
+selectbox_options = [
+    "Caixa de Som",
+    "Celular",
+    "Fone",
+    "Notebook",
+    "Smartwatch",
+    "Tablet",
+    "Teclado",
+    "TV"
+]
+selected_category = st.sidebar.selectbox(
+    "CATEGORIAS",
+    selectbox_options,
+    index=0
+)
 row6_col1, row6_col2 = st.columns(2, gap="medium")
 
 with row6_col1:
-    st.markdown("#### 🔵 MAGAZINE LUIZA")
+    st.markdown("#### 🔵 Magazine Luiza")
     st.markdown(f"##### ➡️ Categoria Selecionada: {selected_category.upper()}")
 
-    magalu_price_variance = mysql_service.get_category_price_variance_over_time(
+    magalu_price_variance = mysql_service.get_avg_category_prices_over_time(
         marketplace="magalu",
         category=selected_category.lower()
     )
 
-    if magalu_price_variance:
-        magalu_price_variance_fig = chart_factory.create_time_series_chart(
-            xlabel="date",
-            ylabel="avg_price",
-            display_xlabel="DATA DA COLETA",
-            display_ylabel="PREÇO MÉDIO",
-            chart_title="",
-            source=magalu_price_variance
-        )
-        st.pyplot(magalu_price_variance_fig, width="stretch")
-    else:
-        st.markdown(
-            "<h4 style='text-align: center;'>🤷‍♂️ Categoria inexistente</h4>",
-            unsafe_allow_html=True
-        )
+    st.line_chart(
+        data=magalu_price_variance,
+        color="#1E90FF"  # dodger blue
+    )
 
 with row6_col2:
-    st.markdown("#### 🟡 MERCADO LIVRE")
+    st.markdown("#### 🟡 Mercado Livre")
     st.markdown(f"##### ➡️ Categoria selecionada: {selected_category.upper()}")
 
-    ml_price_variance = mysql_service.get_category_price_variance_over_time(
+    ml_price_variance = mysql_service.get_avg_category_prices_over_time(
         marketplace="mercado_livre",
         category=selected_category.lower()
     )
 
-    if ml_price_variance:
-        ml_price_variance_fig = chart_factory.create_time_series_chart(
-            xlabel="date",
-            ylabel="avg_price",
-            display_xlabel="DATA DA COLETA",
-            display_ylabel="PREÇO MÉDIO",
-            chart_title="",
-            source=ml_price_variance
-        )
-        st.pyplot(ml_price_variance_fig, width="stretch")
-    else:
-        st.markdown(
-            "<h4 style='text-align: center;'>🤷‍♂️ Categoria inexistente</h4>",
-            unsafe_allow_html=True
-        )
+    st.line_chart(
+        data=ml_price_variance,
+        color="#FFEE8C"  # pastel yellow
+    )
